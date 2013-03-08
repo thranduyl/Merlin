0b5b73b3c8052dc55fb2448ece832c29f22f091d
Merlin <thranduyl@hotmail.com>
2013-03-08 19:12:31 +0100

    recogida

--- html/resultado.php
+++ html/resultado.php
@@ -6,20 +6,21 @@
 
     <BODY>
 	<?php
-	$nombre = $_POST["nombre"];
-	$apellido = $_POST["apellido"];
-	$Genero = $_POST["Genero"];
-	$Edad = $_POST["edad"];
-	/*$favoritos =$_POST["favoritos1"];
-	$favoritos2 =$_POST["favoritos2"];
-	$favoritos3 =$_POST["favoritos3"];
-	$favoritos4 =$_POST["favoritos4"];
-	$favoritos5 =$_POST["favoritos5"];
-	$favoritos6 =$_POST["favoritos6"];*/
-	$comentario =$_POST["Comentario"];
+	$nombre = $_POST;
+	
+	
+	foreach ($nombre as $key=> $value){
+		echo $value;
+		if($key)
+	
+	
+	
+	
+	}
+	
     
 		
-	echo $nombre." ".$apellido." ".$Genero." ".$Edad." ".$comentario ?>
+ ?>
     
  
     </BODY>
